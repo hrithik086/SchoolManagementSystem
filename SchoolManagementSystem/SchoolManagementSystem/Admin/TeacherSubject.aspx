52dabb2ec3f6498224e688ff4732c062b25b9798
hrithik <hrithik086@gmail.com>
2023-10-22 18:12:24 +0530

    Add Teacher Student page with insert, update and delete operation added

---
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/TeacherSubject.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdmintMst.Master" AutoEventWireup="true" CodeBehind="TeacherSubject.aspx.cs" Inherits="SchoolManagementSystem.Admin.TeacherSubject" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div style="background-image:url('../Images/Color Pencils.jpg'); width: 100%; height: 720px; background-repeat: no-repeat; background-size: cover; background-attachment: fixed;">
+        <div class="container p-md-4 p-sm-4">
+            <div>
+                <asp:Label ID="lblMessage" runat="server"></asp:Label>
+            </div>
+            <h2 class="text-center">Add Teacher Subject</h2>
+
+            <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-5">
+                <div class="col md-6">
+                    <label for="ddlClass">Class</label>
+                    <asp:DropDownList ID="ddlClass" runat="server" CssClass="form-control" AutoPostBack="true" OnSelectedIndexChanged="ddlClass_SelectedIndexChanged"></asp:DropDownList>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Class is Required" 
+                        ControlToValidate="ddlClass" Display="Dynamic" ForeColor="Red" InitialValue="Select Class" SetFocusOnError="true">
+                    </asp:RequiredFieldValidator>
+                </div>
+                <div class="col md-6">
+                    <label for="ddlSubject">Subject</label>
+                    <asp:DropDownList ID="ddlSubject" runat="server" CssClass="form-control"></asp:DropDownList>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ErrorMessage="Subject is Required" 
+                        ControlToValidate="ddlSubject" Display="Dynamic" ForeColor="Red" InitialValue="Select Subject" SetFocusOnError="true">
+                    </asp:RequiredFieldValidator>
+                </div>
+                <div class="col md-6">
+                    <label for="ddlTeacher">Teacher</label>
+                    <asp:DropDownList ID="ddlTeacher" runat="server" CssClass="form-control"></asp:DropDownList>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ErrorMessage="Teacher is Required" 
+                        ControlToValidate="ddlTeacher" Display="Dynamic" ForeColor="Red" InitialValue="Select Teacher" SetFocusOnError="true">
+                    </asp:RequiredFieldValidator>
+                </div>
+            </div>
+
+            <div class="row mb-3 mr-lg-5 ml-lg-5>
+                <div class="col-md-3 col-md-offset-2 mb-3">
+                    <asp:Button ID="btnAdd" runat="server" CssClass="btn btn-primary btn-block" BackColor="#5558C9" Text="Add Subject" Width="745px" OnClick="btnAdd_Click" />
+                </div>
+            </div> 
+
+            <div class="row mb-3 mr-lg-5 ml-lg-5>
+                <div class="col-md-6" style="margin-left: 50%;">
+                    <asp:GridView ID="GridView1" runat="server" DataKeyNames="Id" AutoGenerateColumns="False" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating">
+                        <Columns>
+                            <asp:TemplateField HeaderText="ClassId">
+                                <EditItemTemplate>
+                                    <asp:TextBox ID="txtClass" runat="server" Text='<%# Eval("ClassId") %>'></asp:TextBox>
+                                </EditItemTemplate>
+                                <ItemTemplate>
+                                    <asp:Label ID="Label1" runat="server" Text='<%# Eval("ClassId") %>'></asp:Label>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="SubjectId">
+                                <EditItemTemplate>
+                                    <asp:TextBox ID="txtSubject" runat="server" Text='<%# Eval("SubjectId") %>'></asp:TextBox>
+                                </EditItemTemplate>
+                                <ItemTemplate>
+                                    <asp:Label ID="Label2" runat="server" Text='<%# Eval("SubjectId") %>'></asp:Label>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="TeacherId">
+                                <EditItemTemplate>
+                                    <asp:TextBox ID="txtTeacher" runat="server" Text='<%# Eval("TeacherId") %>'></asp:TextBox>
+                                </EditItemTemplate>
+                                <ItemTemplate>
+                                    <asp:Label ID="Label3" runat="server" Text='<%# Eval("TeacherId") %>'></asp:Label>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:CommandField HeaderText="Operations" ShowEditButton="True" CausesValidation="False" />
+                        </Columns>
+                        <HeaderStyle BackColor="#5558C9" ForeColor="White" />
+                    </asp:GridView>
+                </div>
+            </div>
+
+
+
+
+        </div>
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+</asp:Content>
---
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/TeacherSubject.aspx.cs
@@ -0,0 +1,169 @@
+﻿using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class TeacherSubject : System.Web.UI.Page
+    {
+        CommonFnx fn= new CommonFnx();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetClass();
+                GetTeacher();
+                GetTeacherSubject();
+            }
+            
+        }
+
+        protected void ddlClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(IsPostBack)
+            {
+                string classId = ddlClass.SelectedValue;
+                GetSubject(classId);
+            }
+        }
+
+        private void GetClass()
+        {
+            string query = "select [ClassId], [ClassName] from Class";
+            try
+            {
+                DataTable dt = fn.Fetch(query);
+                ddlClass.DataSource = dt;
+                ddlClass.DataValueField = "ClassId";
+                ddlClass.DataTextField = "ClassName";
+                ddlClass.DataBind();
+                ddlClass.Items.Insert(0, "Select Class");
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>'Exceptin while fetching Class Details " + ex.Message + "'</script>");
+            }
+        }
+
+        private void GetSubject(string classId)
+        {
+            string query = "select [SubjectId], [ClassId], [SubjectName] from Subject where ClassId = '" + classId + "'";
+
+            try
+            {
+                DataTable dt = fn.Fetch(query);
+                ddlSubject.DataSource = dt;
+                ddlSubject.DataTextField = "SubjectName";
+                ddlSubject.DataValueField = "SubjectId";
+                ddlSubject.DataBind();
+                ddlSubject.Items.Insert(0, "Select Subject");
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>'Error occured while fetching Student Details exception - " + ex.Message + "'</script>");
+            }
+        }
+
+        private void GetTeacher()
+        {
+            string query = "select [TeacherId], [Name] from Teacher";
+
+            try
+            {
+                DataTable dt = fn.Fetch(query);
+                ddlTeacher.DataSource = dt;
+                ddlTeacher.DataTextField = "Name";
+                ddlTeacher.DataValueField = "TeacherId";
+                ddlTeacher.DataBind();
+                ddlTeacher.Items.Insert(0, "Select Teacher");
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>'Error has occured while fetching Teacher details exception - " + ex.Message + "'</script>");
+            }
+        }
+
+        private void GetTeacherSubject()
+        {
+            string query = "select [Id] ,[ClassId], [SubjectId], [TeacherId] from TeacherSubject";
+
+            try
+            {
+                DataTable dt = fn.Fetch(query);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>'Exception occured while fetching exception - " + ex.Message + "'</script>");
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string classId = ddlClass.SelectedValue;
+            string subjectId = ddlSubject.SelectedValue;
+            string teacherId = ddlTeacher.SelectedValue;
+            string query = "Insert into TeacherSubject values('" +
+                classId + "', '" +
+                subjectId + "', '" +
+                teacherId + "')";
+
+            try
+            {
+                fn.Query(query);
+                lblMessage.Text = "Teacher subject added successfully";
+                lblMessage.CssClass = "alert alert-success";
+                GetTeacherSubject();
+            }
+            catch(Exception ex)
+            {
+                lblMessage.Text = "exception occured while inserting";
+                lblMessage.CssClass = "alert alert-danger";
+                Response.Write("<script>'exception occured while insering exception -" + ex.Message + "'</script>");
+            }
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            GetTeacherSubject();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            string classId = (row.FindControl("txtClass") as TextBox).Text;
+            string subjectId = (row.FindControl("txtSubject") as TextBox).Text;
+            string teacherId = (row.FindControl("txtTeacher") as TextBox).Text;
+            string id= GridView1.DataKeys[e.RowIndex].Values[0].ToString();
+            string query = "update TeacherSubject set ClassId = '" + classId + "'," +
+                "SubjectId = '" + subjectId + "'," +
+                "TeacherId = '" + teacherId + "' where id = '" + id + "'";
+            try
+            {
+                fn.Query(query);
+                lblMessage.Text = "Edited Successfully";
+                lblMessage.CssClass = "alert alert-success";
+                GridView1.EditIndex = -1;
+                GetTeacherSubject();
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>'Exception occured while updating exception - " + ex.Message + "'</script>");
+            }
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            GetTeacherSubject();
+        }
+    }
+}
